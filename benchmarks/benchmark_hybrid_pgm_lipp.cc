e7c30bcc42810d3adaeb9eb74ff5127cdf13c27b
arushig11 <arushi.gupta2024@gmail.com>
2025-04-21 19:12:54 -0400

    benchmark fix

--- benchmarks/benchmark_hybrid_pgm_lipp.cc
+++ benchmarks/benchmark_hybrid_pgm_lipp.cc
@@ -4,12 +4,12 @@
 #include "benchmarks/common.h"
 #include "competitors/hybrid_pgm_lipp.h"
 
-void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark) {
-    benchmark.template Run<HybridPGMLIPP<uint64_t>>();
-}
-
 int main(int argc, char** argv) {
+    // Initialize Benchmark with arguments
     tli::Benchmark<uint64_t> benchmark;
     benchmark::InitBenchmark(benchmark, argc, argv);
+
+    // Run the hybrid benchmark
     benchmark_64_hybrid_pgm_lipp(benchmark);
+    return 0;
 }
